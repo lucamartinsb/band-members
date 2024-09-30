c389ce48423c522849d89df030d95ccfe19d7681
lucamartinsb <lucabindermartins@gmail.com>
2024-09-30 14:27:45 -0300

    fix vue script / create new members

--- scripts.js
+++ scripts.js
@@ -15,10 +15,21 @@ const handlingForms = {
 
     data() {
         return {
-            members: window.members
+            members: window.members,
+            newMember: {}
         }
     },
-
-};
+    methods: {
+        addMember: function () {
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+                this.members.push(this.newMember)
+                this.newMember = {}
+            }
+            else {
+                window.alert('Todos os campos devem ser preenchidos!')
+            }
+        }
+    }
+}
 
 Vue.createApp(handlingForms).mount('#app');
